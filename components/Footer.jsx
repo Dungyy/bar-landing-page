dbc25f5faec2ce7b3798b2dd3c98c616ba1cd783
Dungyy <erickmunoz13@gmail.com>
2022-04-04 03:54:40 -0500

    Satrted Nav | HERO of Bar website

---
+++ components/Footer.jsx
@@ -0,0 +1,22 @@
+import Link from "next/link";
+import styles from "../styles/footer.module.css";
+import { BsPhone } from "react-icons/Bs";
+import { FaMapMarkerAlt } from "react-icons/Fa";
+import { AiOutlineMail } from "react-icons/Ai";
+
+
+
+
+const Footer = () => {
+  return (
+    <div className={styles.container}>
+      <Link href="/">© Copyright 2021 Dungys Bar</Link>
+        <p><BsPhone style={{fontSize: "30px"}}/> (320) 333-3214</p>
+        
+      <p> <FaMapMarkerAlt style={{fontSize: "30px"}}/> Montevideo, Uruguay 56265</p>
+      <p> <AiOutlineMail style={{fontSize: "30px"}}/><Link href=""> DungysLiquorLLC@live.com </Link></p>
+    </div>
+  );
+};
+
+export default Footer;
